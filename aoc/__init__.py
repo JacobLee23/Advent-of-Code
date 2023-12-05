99de5efc416991cfa01ff35d9d3e9338ba4539eb
Jacob Lee <JLpython@outlook.com>
2023-12-05 13:41:24 -0800

    feat: create logger

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -2,9 +2,19 @@
 """
 
 import argparse
+import logging
 import os
 import pathlib
 import shutil
+
+
+LOGGER = logging.getLogger(__name__)
+STREAM_HANDLER = logging.StreamHandler()
+FORMATTER = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+STREAM_HANDLER.setFormatter(FORMATTER)
+LOGGER.addHandler(STREAM_HANDLER)
+
+LOGGER.setLevel(logging.DEBUG)
 
 
 class AdventOfCode:
@@ -34,9 +44,6 @@ class AdventOfCode:
             os.mkdir(self.events)
 
         self.arguments = self.parser.parse_args()
-        directory = self.arguments.func(self.arguments)
-
-        print(directory)
 
     def init(self, args: argparse.Namespace) -> pathlib.Path:
         """
@@ -45,12 +52,18 @@ class AdventOfCode:
         """
         directory = self.events / str(args.year)
         if directory.exists():
+            LOGGER.info(
+                "Directory already exists (force=%s): %s", self.arguments.force, directory
+            )
+
             if args.force:
                 shutil.rmtree(directory)
+                LOGGER.info("Removed directory and children: %s", directory)
             else:
                 raise FileExistsError(directory)
 
         os.mkdir(directory)
+        LOGGER.info("Created directory: %s", directory)
 
         for day in range(1, 26):
             subdirectory = directory / str(day).zfill(2)
@@ -62,6 +75,8 @@ class AdventOfCode:
             with open(subdirectory / "solution.py", "w+", encoding="utf-8") as file:
                 file.write(file.read().format(year=args.year, day=day))
 
+            LOGGER.info("Copied directory: %s -> %s", self.template, subdirectory)
+
         return directory
 
     def remove(self, args: argparse.Namespace) -> pathlib.Path:
@@ -72,12 +87,17 @@ class AdventOfCode:
         """
         directory = self.events / str(args.year)
         if not directory.exists():
+            LOGGER.info("Directory already exists: %s", directory)
             raise FileNotFoundError(directory)
 
         shutil.rmtree(directory)
+        LOGGER.info("Removed directory: %s", directory)
 
         return directory
 
 
 def main():
     aoc = AdventOfCode()
+    directory = aoc.arguments.func(aoc.arguments)
+
+    print(directory.absolute())
