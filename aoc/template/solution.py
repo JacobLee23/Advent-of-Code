da5ee480d27df8e83ac2f266f349a4a56956416f
Jacob Lee <JLpython@outlook.com>
2023-12-05 13:12:06 -0800

    feat: create template directory for challenge solutions

---
+++ aoc/template/solution.py
@@ -0,0 +1,17 @@
+"""
+Solution for Advent of Code {year}, Day {day}
+"""
+
+import typing
+
+
+def part1(input: str) -> typing.Any:
+    """
+    Solution for Part 1
+    """
+
+
+def part2(input: str) -> typing.Any:
+    """
+    Solution for Part 2
+    """
