46d738a23a5cdcff2ad6b2cb9056646d0c465946
Jacob Lee <JLpython@outlook.com>
2023-12-06 08:52:30 -0800

    debug: create command line argument to adjust logging verbosity

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -14,8 +14,6 @@ FORMATTER = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 STREAM_HANDLER.setFormatter(FORMATTER)
 LOGGER.addHandler(STREAM_HANDLER)
 
-LOGGER.setLevel(logging.DEBUG)
-
 
 class AdventOfCode:
     """
@@ -28,6 +26,7 @@ class AdventOfCode:
         self.parser = argparse.ArgumentParser(
             prog=__name__, description=__doc__, epilog="Advent of Code"
         )
+        self.parser.add_argument("-v", "--verbose", action="count", default=0)
 
         subparsers = self.parser.add_subparsers(dest="name")
 
@@ -39,6 +38,12 @@ class AdventOfCode:
         paser_remove = subparsers.add_parser("remove", aliases=["rm"])
         paser_remove.add_argument("year", type=int)
         paser_remove.set_defaults(func=self.remove)
+
+        parser_solve = subparsers.add_parser("solve", aliases=["s"])
+        parser_solve.add_argument("year", type=int)
+        parser_solve.add_argument("-p", "--part", type=int, choices=[1, 2])
+        parser_solve.add_argument("-t", "--test", action="store_true")
+        parser_solve.set_defaults(func=self.solve)
 
         if not self.events.exists():
             os.mkdir(self.events)
@@ -52,18 +57,18 @@ class AdventOfCode:
         """
         directory = self.events / str(args.year)
         if directory.exists():
-            LOGGER.info(
+            LOGGER.debug(
                 "Directory already exists (force=%s): %s", self.arguments.force, directory
             )
 
             if args.force:
                 shutil.rmtree(directory)
-                LOGGER.info("Removed directory and children: %s", directory)
+                LOGGER.debug("Removed directory and children: %s", directory)
             else:
                 raise FileExistsError(directory)
 
         os.mkdir(directory)
-        LOGGER.info("Created directory: %s", directory)
+        LOGGER.debug("Created directory: %s", directory)
 
         for day in range(1, 26):
             subdirectory = directory / str(day).zfill(2)
@@ -75,7 +80,7 @@ class AdventOfCode:
             with open(subdirectory / "solution.py", "w+", encoding="utf-8") as file:
                 file.write(file.read().format(year=args.year, day=day))
 
-            LOGGER.info("Copied directory: %s -> %s", self.template, subdirectory)
+            LOGGER.debug("Copied directory: %s -> %s", self.template, subdirectory)
 
         return directory
 
@@ -87,17 +92,34 @@ class AdventOfCode:
         """
         directory = self.events / str(args.year)
         if not directory.exists():
-            LOGGER.info("Directory already exists: %s", directory)
+            LOGGER.debug("Directory already exists: %s", directory)
             raise FileNotFoundError(directory)
 
         shutil.rmtree(directory)
-        LOGGER.info("Removed directory: %s", directory)
+        LOGGER.debug("Removed directory: %s", directory)
 
         return directory
+    
+    def solve(self, args: argparse.Namespace) -> None:
+        """
+        :param args:
+        """
 
 
 def main():
     aoc = AdventOfCode()
-    directory = aoc.arguments.func(aoc.arguments)
+    
+    if aoc.arguments.verbose == 0:
+        LOGGER.setLevel(logging.WARNING)
+    elif aoc.arguments.verbose == 1:
+        LOGGER.setLevel(logging.INFO)
+    else:
+        LOGGER.setLevel(logging.DEBUG)
 
-    print(directory.absolute())
+    result = aoc.arguments.func(aoc.arguments)
+    if aoc.arguments.name == "init":
+        LOGGER.info("Initialized: %s", result)
+    elif aoc.arguments.name == "remove":
+        LOGGER.info("Removed: %s", result)
+    elif aoc.arguments.name == "solve":
+        LOGGER.info(result)
