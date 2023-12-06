9d3bb4e2e77d835576d2cbe0db649a47d054f4a9
Jacob Lee <JLpython@outlook.com>
2023-12-06 15:36:34 -0800

    feat: solve Day 2

--- events/aoc2023/day02/solution.py
+++ events/aoc2023/day02/solution.py
@@ -2,16 +2,47 @@
 Solution for Advent of Code 2023, Day 2
 """
 
-import typing
+import functools
+import operator
+import re
 
 
-def part1(input: str) -> typing.Any:
+CONTENTS = {"red": 12, "green": 13, "blue": 14}
+
+
+def part1(input: str) -> int:
     """
     Solution for Part 1
     """
+    result = 0
+
+    for line in input.split("\n"):
+        game, record = re.search(r"^Game (\d+): (.*)$", line).groups()
+
+        subsets = [
+            {b: int(a) for a, b in re.findall(r"(\d+) (red|blue|green)", x)}
+            for x in record.split(";")
+        ]
+        if all(cubes.get(k, 0) <= v for k, v in CONTENTS.items() for cubes in subsets):
+            result += int(game)
+
+    return result
 
 
-def part2(input: str) -> typing.Any:
+def part2(input: str) -> int:
     """
     Solution for Part 2
     """
+    result = 0
+
+    for line in input.split("\n"):
+        _, record = re.search(r"^Game (\d+): (.*)$", line).groups()
+
+        subsets = [
+            {b: int(a) for a, b in re.findall(r"(\d+) (red|blue|green)", x)}
+            for x in record.split(";")
+        ]
+        maxcubes = {k: max(x.get(k, 0) for x in subsets) for k in CONTENTS}
+        result += functools.reduce(operator.mul, maxcubes.values())
+
+    return result
